73e66b37be7d23b68ac64b17ce3764d0cee1f901
luis-ojeda <luisjojeda1@gmail.com>
2014-10-08 15:32:43 -0300

    metadataaaa

--- workspace/Tarea/src/Cliente/Client.java
+++ workspace/Tarea/src/Cliente/Client.java
@@ -7,32 +7,12 @@ import Server.ServerMulticaster;
 import Server.ServerTCP;
 
 public class Client {
-	
+	Client(){
+	}
 	
 	 public static void main(String args[]) {
-<<<<<<< HEAD
-	    	//Date cumpleaños = new Date(1963, 8, 30);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of c951bd6... Revert "Se modularizo el servidor"
-=======
+
 	    	//Date cumpleanos = new Date(1963, 8, 30);
-		 ServerGUI servergui = new ServerGUI("ServerGUI");
-		 
->>>>>>> origin/master
-<<<<<<< HEAD
-=======
-		 ServerGUI servergui = new ServerGUI("ServerGUI");
-		 
->>>>>>> parent of fdefd2b... Se modularizo el servidor
-=======
-		 ServerGUI servergui = new ServerGUI("ServerGUI");
-		 
->>>>>>> parent of fdefd2b... Se modularizo el servidor
-=======
->>>>>>> parent of c951bd6... Revert "Se modularizo el servidor"
 		 
 		 TcpCliente tcpcliente = new TcpCliente("Tcpcliente");
 		 ChatClient chatclient = new ChatClient("chatclient");
